5b882b5bc9f2e921f50b8440ffd498955235e425
Zeynel <76682637+zeynelabidinozdamar@users.noreply.github.com>
2021-06-11 14:19:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+
+function go()
+{
+	window.location.href = "https://mail.google.com/mail/u/0/#inbox?compose=SxfkdrJDglcnSkjQdzlxqPDCBtCgBPxDsPlRRbrGkbpPVmTmQbmRrKWFGJlwZjrmvjBKCwJswbTlfrqrgrvbfxtXMZvzRpvNwdQlnGDmjqJmGdZxpsq";
+	
+}
