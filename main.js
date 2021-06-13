b2247ac5250aeaa8ca253ef897e930c681516532
Zeynel <76682637+zeynelabidinozdamar@users.noreply.github.com>
2021-06-13 19:22:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ console.log('Hello World!');
 
 function go()
 {
-	window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSc2gW7fEOALAHCR7A2LIFfKlYQ8TZRmqDwme1LYy5idGOK7gA/viewform?usp=sf_link";
+	window.location.href = "http://zeynelozdamar.tk";
 	
 }
