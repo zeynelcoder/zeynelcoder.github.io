138a2e6259b0a9d427b565325bbe87850cbe99b3
Zeynel <76682637+zeynelabidinozdamar@users.noreply.github.com>
2021-06-13 19:36:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 console.log('Hello World!');
 
 function go()
+
 {
-	window.location.href = "http://zeynelozdamar.tk";
+
+	window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSc2gW7fEOALAHCR7A2LIFfKlYQ8TZRmqDwme1LYy5idGOK7gA/viewform?usp=sf_link";
+
 	
+
 }
