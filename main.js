f9deed5cd7f63d4888256007524690c4f0d3fe2b
Zeynel <76682637+zeynelabidinozdamar@users.noreply.github.com>
2022-05-21 15:11:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+var menubox=document.getElementById('menubox');
+var b=document.getElementById('body');
+
+function menuop(){
+menubox.style.display= 'block';
+}
+
+function menuof(){
+menubox.style.display= 'none';
+}
