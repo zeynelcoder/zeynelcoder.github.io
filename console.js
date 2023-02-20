8d0e3a1210a8c63d22c67420f26da2f777fd408b
Zeynel Abidin Özdamar <76682637+zeynelabidinozdamar@users.noreply.github.com>
2023-02-20 20:05:33 +0300

    Add files via upload

---
+++ console.js
@@ -0,0 +1,14 @@
+x = document.getElementById("input");
+
+v1="ben.onun.balıyım";
+v2="incir.reçelimi.göster.bana";
+
+function e(){
+if(x.value==v1){
+alert("Zeynel varya sana deliler gibi aşık, hem de ne biçim!")
+}
+if(x.value==v2){
+   alert("Hemen Efendim!")
+   window.location.href="http://zeynel.tk/img/img2.jpeg";
+}
+}
