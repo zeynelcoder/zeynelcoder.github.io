0cfd04dec1a7bda4ac3bd11237d4a40f541a6bc2
Zeynel Abidin Özdamar <76682637+zeynelabidinozdamar@users.noreply.github.com>
2023-03-25 22:12:32 +0300

    Add files via upload

--- coolclock/main.js
+++ coolclock/main.js
@@ -29,67 +29,53 @@ setInterval(function(){
 }, 1)
 
 var dies="#";
-var colors=["#fff","#FF4848","#C2FFD9","#142F43","#FFE459","000"];
+//         beyaz 0  siyah 1  yeşil 2  turuncu 3   mor 4            kırmızı 5
+var colors=["#fff","#000","#90ee90","darkorange","rgba(157,24,72)","rgba(237,30,44)"];
 var sel=0;
 
 function selup(){
 	sel++;
-	if(sel>6)
+	if(sel>5)
 	{
 		sel=0;console.log("Sel Değişkeni Sıfırlandı. Yeni Değer; 0")
 	}
 	console.log(sel)
 	
 	switch (sel) {
+		// beyaz siyah
 		case 0:
-		document.getElementById("body").style.backgroundColor= colors[1];
-		document.getElementById("body").style.color= colors[2];
+		document.getElementById("body").style.backgroundColor= colors[0];
+		document.getElementById("body").style.color= colors[1];
 		document.getElementById('cover').style.borderColor= colors[2];
 		break;
+		// siyah beyaz
 		case 1:
-			document.getElementById("body").style.backgroundColor = colors[2];
-		document.getElementById("body").style.color = colors[3];
+		document.getElementById("body").style.backgroundColor = colors[1];
+		document.getElementById("body").style.color = colors[0];
 		document.getElementById('cover').style.borderColor= colors[3];
 		break;
+		// siyah yeşil
 		case 2:
-			document.getElementById("body").style.backgroundColor = colors[3];
-		document.getElementById("body").style.color = colors[4];
+		document.getElementById("body").style.backgroundColor = colors[1];
+		document.getElementById("body").style.color = colors[2];
 		document.getElementById('cover').style.borderColor= colors[4];
 		break;
+		// siyah turuncu
 		case 3:
-			document.getElementById("body").style.backgroundColor = 'darkorange';
-		document.getElementById("body").style.color = 'white';
+			document.getElementById("body").style.backgroundColor = colors[1];
+		document.getElementById("body").style.color = colors[3];
 		document.getElementById('cover').style.borderColor= 'white';
 		break;
 		case 4:
-			document.getElementById("body").style.backgroundColor = 'black';
-		document.getElementById("body").style.color = '#90ee90';
-		document.getElementById('cover').style.borderColor= '#90ee90';
+			document.getElementById("body").style.backgroundColor = colors[1];
+		document.getElementById("body").style.color = colors[4];
+		document.getElementById('cover').style.borderColor= 'white';
 		break;
 		case 5:
-			document.getElementById("body").style.backgroundColor = colors[0];
-		document.getElementById("body").style.color = 'black';
-		document.getElementById('cover').style.borderColor= 'black';
-		break;
+			document.getElementById("body").style.backgroundColor = colors[1];
+		document.getElementById("body").style.color = colors[5];
+		document.getElementById('cover').style.borderColor= 'white';
+		
+
 	}
 }
-
-
-
-
-
-
-
-
-
-
-// tek haneli sayıya 0 ekleme
-/*
-var c = "ddddghk"
-return dakika.toString().length;
-if (dakika.length < 10) {
-	alert("hi");
-	console.log("ejherjt")
-}
-*/
-
