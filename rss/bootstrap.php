aa8bbfd09c3d8eca51df9f7a81fd011dbc6c978d
Zeynel Abidin Özdamar <76682637+zeynelcoder@users.noreply.github.com>
2023-09-08 01:28:14 +0300

    Delete rss directory

--- rss/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-const COPY_LOG_TO_SYSLOG = false;
-
-require(__DIR__ . '/../constants.php');
-require(LIB_PATH . '/lib_rss.php');	//Includes class autoloader
